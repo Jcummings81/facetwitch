13a11502b82456c14329a93d4b63aa537b11a409
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-23 18:32:00 -0600

    updated gem routes and seedfile and filled out api controller

--- app/controllers/api/twitches_controller.rb
+++ app/controllers/api/twitches_controller.rb
@@ -1,16 +1,41 @@
 class Api::TwitchesController < ApplicationController
+  before_action :set_twitch, only: [:show, :update, :destroy]
+
   def index
+    render json: Twitch.all.order(created_at: :desc)
   end
 
   def show
+    render json: @twitch
   end
 
   def create
+    twitch = Twitch.create(twitch_params)
+    if twitch.save
+      render json: twitch
+    else
+      render json: { errors: twitch.errors.full_messages.join(','), status: 422}
+    end
   end
 
   def update
+    if @twitch.update(twitch_params)
+      render json: @twitch
+    else
+      render json: { errors: @twitch.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def destroy
+    @twitch.destroy
+  end
+
+  private
+  def set_twitch
+    @twitch = Twitch.find(params[:id])
+  end
+
+  def twitch_params
+    params.require(:twitch).permit(:post, :liked)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+    Twitch.create(
+        post: Faker::Lorem.paragraph(2),
+        liked: false
+    )
+end
